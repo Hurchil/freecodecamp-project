d7e5b8d428d0750b49f8ed293c7fab1b6f4fbca1
hurchil <tchindadjate@gmail.com>
2024-09-06 07:44:02 +0100

    Finis !

---
+++ backend-developpement-and-apis/project-1/index.js
@@ -0,0 +1,77 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invaid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+});
+
+
+
+app.get('/api/:time', (req, res) => {
+
+  let newTime = null;
+  const params = req.params.time;
+  
+  if(!isNaN(Number(params)) && /^\d{5,}/.test(params)){
+    newTime = new Date(Number(params));
+  }
+  else if(!(new Date(params).toString() === "Invalid Date")){
+    newTime = new Date(params);
+  }
+  else{
+    res.json({error: "Invalid Date"});
+    return;
+  }
+  res.json({"unix": newTime.getTime(), "utc": newTime.toUTCString()});
+})
+
+app.get('/api/', (req, res) => {
+  let newTime = new Date();
+  res.json({"unix": newTime.valueOf(), "utc": newTime.toUTCString()});
+})
